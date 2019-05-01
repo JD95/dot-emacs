e8f0aaf2059c1c6221d52d04205149dc27237f0b
Jeff Dwyer <jdwyer@voceracommunications.com>
2019-05-01 11:19:51 -0700

    Adds flags to enable major modes in settings.el

--- default-settings.el
+++ default-settings.el
@@ -9,3 +9,15 @@
 (setq find-location "")
 
 (setq browser-path "")
+
+(setq config-use-flyspell nil)
+(setq config-use-magit nil)
+(setq config-use-elfeed nil)
+(setq config-use-coq nil)
+(setq config-use-haskell nil)
+(setq config-use-idris nil)
+(setq config-use-purescript nil)
+(setq config-use-python nil)
+(setq config-use-nix nil)
+(setq config-use-rust nil)
+(setq config-use-javascript nil)
