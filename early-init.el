a150e0d4cee3a7a67217ad90c54807a3ca4e8bc0
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2023-06-20 16:58:07 -0700

    Fixes configure-require bugs and fixes warnings

--- early-init.el
+++ early-init.el
@@ -1,2 +1,11 @@
 ;; Ensure no GC in startup
 (setq gc-cons-threshold most-positive-fixnum)
+(defun config-require (symbol)
+  (if (boundp symbol)
+      (if (eval symbol) "yes" "no")
+      (progn
+        (with-temp-buffer
+          (insert-file-contents (concat user-emacs-directory "settings.el"))
+          (insert (concat "(setq " (symbol-name symbol) " nil)\n"))
+          (write-region (point-min) (point-max) (concat user-emacs-directory "settings.el")))
+        "no")))
---
+++ settings.el
@@ -0,0 +1,38 @@
+(setq idris-location "")
+
+(setq haskell-stack-location "")
+
+(setq find-location "")
+
+(setq browser-path "c:/Users/jeffr/AppData/Local/Vivaldi/Application/vivaldi.exe")
+
+(setq org-agenda-files
+      '("e:/nextcloud/logs.org"
+        "e:/nextcloud/projects.org"
+        "e:/nextcloud/individual-tasks.org"
+        "e:/nextcloud/repeating.org"
+        "e:/nextcloud/social.org"
+        "e:/nextcloud/reminders.org"
+        "e:/nextcloud/refinement.org"
+        "e:/nextcloud/events.org"
+        ))
+(setq org-refile-targets
+      '((org-agenda-files :maxlevel . 1)))
+
+(setq org-zettelkasten-dir "e:/nextcloud/Zettlekasten")
+(setq zettelkasten-journal-path "e:/nextcloud/Zettlekasten/conversations/journal.org")
+(setq zettelkasten-people-path "e:/nextcloud/Zettlekasten/17062020123421.org")
+(setq z3-solver-cmd "z3")
+
+(setq use-agda nil)
+(setq use-z3 nil)
+(setq use-rust nil)
+(setq use-nix nil)
+(setq use-javascript nil)
+(setq use-php nil)
+(setq use-python nil)
+(setq use-purescript nil)
+(setq use-idris nil)
+(setq use-haskell t)
+(setq use-magit t)
+(setq use-flyspell nil)
